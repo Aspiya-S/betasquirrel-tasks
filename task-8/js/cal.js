bf968549f19bb7c7b52983cf0d785a91dccd8989
Aspiya-S <aspiyasyed.as@gmail.com>
2023-03-14 14:25:01 +0400

    task added

---
+++ task-8/js/cal.js
@@ -0,0 +1,19 @@
+function Solve()
+const number1 = document.getElementById("res")
+innervalue = "Solve";
+const number2 = document.getElementById("res")
+innervalue = "Solve";
+const operator = document.getElementById("res")
+innervalue = "Solve";
+let result;
+if(operator == '+') 
+{result = number1 + number2;
+}else if(operator == '-') 
+{result = number1 - number2; 
+}else if(operator == '*') 
+{result = number1 * number2; 
+}else if(operator == '/') 
+{result = number1 / number2; 
+}else
+{result = number1;
+}
